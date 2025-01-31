069fc23b82d0846ae4fce3479b0459ddb7961100
Meredith Whisman <mwhismain@gmail.com>
2025-01-31 11:11:38 -0800

    more styling

--- madlibs/script.js
+++ madlibs/script.js
@@ -4,3 +4,15 @@
 
     //Add code here...
 })();
+
+//NOTES FOR LATER
+
+//Scroll to section for form
+//JS
+/* function scrollWin() {
+
+    window.scrollTo(h{int}, y{int});
+  }
+
+//HTML
+<button onclick="scrollWin()">Scroll to {Section}</button> */
