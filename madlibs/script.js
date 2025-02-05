9ff26099c19b35e174c9b85e2f2fbb5a7e4f7cee
Meredith Whisman <mwhismain@gmail.com>
2025-02-04 18:33:54 -0800

    fixed JS

--- madlibs/script.js
+++ madlibs/script.js
@@ -11,8 +11,8 @@
         output.scrollIntoView();
     });
 
-    //MadLibs Form
-    const myForm = document.querySelector(`#myform`);
+    //MadLibs Input and Form
+    const myForm = document.querySelector(`#form`);
     const madLib = document.querySelector(`#madlibs-output`);
     const formData = document.querySelectorAll(`input[type=text]`);
 
@@ -29,12 +29,13 @@
         for (const eachWord of formData) {
             if (eachWord.value) {
                 words.push(eachWord.value);
+                eachWord.value = "";
             } else {
                 emptyfields.push(counter);
             }
             counter++;
         }
-        
+
         if (emptyfields.length > 0) {
             showErrors(formData, emptyfields);
         } else {
@@ -45,18 +46,23 @@
     function showErrors(formData, emptyfields) {
         const errorID = formData[emptyfields[0]].id;
         const errorText = `Please fill out this field: ${errorID}.`;
-        //madLib.innerHTML = "";
         madLib.innerHTML = errorText;
         document.querySelector(`#${errorID}`).focus();
     }
 
     function makeMadLib(words) {
-        //const myText = `Here are the words you entered: ${words[0]}, ${words[1]}, ${words[2]}, and ${words[3]}.`;
-        const myText = `They belong to Whiskers, the Biker cat! He loves to rev his <em>${words[0]}</em> motorcycle. He belongs to the <em>${words[1]}</em> gang. They like to go on fun adventures exploring different places, such as <em>${words[2]}</em>. Whiskers is a big foodie so he LOVES eating food. He enjoys eating: <em>${words[3]}</em>, <em>${words[4]}</em>, and <em>${words[5]}</em>. He also loves listening to music! A favorite genre of music he likes to listen to is <em>${words[6]}</em>. Whiskers also enjoys going on <em>${words[7]}</em> rides with his friends.`;
+        const myText = `<p>They belong to Whiskers, the Biker cat! He loves to rev his <em>${words[0]}</em> motorcycle. He belongs to the <em>${words[1]}</em> gang. They like to go on fun adventures exploring different places, such as <em>${words[2]}</em>. Whiskers is a big foodie so he LOVES eating food. He enjoys eating: <em>${words[3]}</em>, <em>${words[4]}</em>, and <em>${words[5]}</em>. He also loves listening to music! A favorite genre of music he likes to listen to is <em>${words[6]}</em>. Whiskers also enjoys going on <em>${words[7]}</em> rides with his friends.</p>`
         madLib.innerHTML = myText;
 
         for (const eachField of formData) {
             eachField.value = "";
         }
     }
+
+    const submitButton = document.querySelector('button[type="submit"]');
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log('Submit button clicked');
+        processFormData(myForm.elements);
+    });
 })();
