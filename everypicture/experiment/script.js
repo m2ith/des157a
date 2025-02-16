e730adeb74c02dac4f7ad1b9588eff0d705513cb
Meredith Whisman <mwhismain@gmail.com>
2025-02-16 11:47:35 -0800

    updated styles and JS

--- everypicture/experiment/script.js
+++ everypicture/experiment/script.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", function() {
-    "ue strict";
+    "use strict";
     console.log("reading JS");
+
+    const posts = document.querySelectorAll(`section`);
+	let postTops = [];
+	let pageTop;
+	let counter = 1;
+	let doneResizing;
 
     const cuijian = new Audio("../music/nothingtomyname_cuijian.mp3");
     const ipod1 = document.querySelector(`#ipod1`);
@@ -11,6 +17,45 @@ window.addEventListener("load", function() {
     let isPlaying = false;
 
     const myImage = document.querySelector(`#myimage`);
+
+    resetPagePosition();
+
+    window.addEventListener("scroll", function () {
+		pageTop = window.scrollY + 300;
+
+		if (pageTop > postTops[counter]) {
+			counter++;
+			//console.log(`scrolling down ${counter}`);
+		}
+		else if (counter > 1 && pageTop < postTops[counter - 1]) {
+			counter--;
+			//console.log(`scrolling up ${counter}`);
+		}
+	});
+
+    window.addEventListener("resize", function () {
+		clearTimeout(doneResizing);
+
+		doneResizing = setTimeout(function () {
+			resetPagePosition();
+		}, 500);
+	});
+
+    function resetPagePosition() {
+		postTops = [];
+		posts.forEach(function(post) {
+			postTops.push(Math.floor(post.getBoundingClientRect().top) + window.scrollY);
+		});
+
+		const pagePosition = window.scrollY + 300;
+		counter = 0;
+
+		postTops.forEach(function(post) { 
+            if (pagePosition > post) { 
+                counter++;
+            } 
+        });
+	}
 
     ipod1.addEventListener("click", function(){
         if (!isPlaying) {
