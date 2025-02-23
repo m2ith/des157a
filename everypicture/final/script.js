5b1a15b800c6f7a926443d1a3c52b6c66661a8bd
Meredith Whisman <mwhismain@gmail.com>
2025-02-23 14:38:28 -0800

    added photos and final every picture

--- everypicture/final/script.js
+++ everypicture/final/script.js
@@ -2,97 +2,93 @@ window.addEventListener("load", function() {
     "use strict";
     console.log("reading JS");
 
-    const posts = document.querySelectorAll(`section`);
-	let postTops = [];
-	let pageTop;
-	let counter = 1;
-	let doneResizing;
-
     const cuijian = new Audio("../music/nothingtomyname_cuijian.mp3");
     const blackkeys = new Audio("../music/goldontheceiling_blackkeys.mp3");
     const beatles = new Audio("../music/yellowsubmarine_thebeatles.mp3");
     const ipod1 = document.querySelector(`#ipod1`);
     const ipod2 = document.querySelector(`#ipod2`);
     const ipod3 = document.querySelector(`#ipod3`);
-    let playingAudio = null;
+    let isCuijianPlaying = false;
+    let isBlackkeysPlaying = false;
+    let isBeatlesPlaying = false;
 
     const myImage = document.querySelector(`#myimage`);
 
-    resetPagePosition();
-
-    window.addEventListener("scroll", function () {
-		pageTop = window.scrollY + 300;
-
-		if (pageTop > postTops[counter]) {
-			counter++;
-			//console.log(`scrolling down ${counter}`);
-		}
-		else if (counter > 1 && pageTop < postTops[counter - 1]) {
-			counter--;
-			//console.log(`scrolling up ${counter}`);
-		}
-	});
-
-    window.addEventListener("resize", function () {
-		clearTimeout(doneResizing);
-
-		doneResizing = setTimeout(function () {
-			resetPagePosition();
-		}, 500);
-	});
-
-    function resetPagePosition() {
-		postTops = [];
-		posts.forEach(function(post) {
-			postTops.push(Math.floor(post.getBoundingClientRect().top) + window.scrollY);
-		});
-
-		const pagePosition = window.scrollY + 300;
-		counter = 0;
-
-		postTops.forEach(function(post) { 
-            if (pagePosition > post) { 
-                counter++;
-            } 
-        });
-	}
-
-    ipod1.addEventListener("click", function(){
-        if (playingAudio = cuijian) {
-            cuijian.play();
-            playingAudio = cuijian;
-            !blackkeys.paused == blackkeys.pause();
-            !beatles.paused == beatles.pause();
-            myImage.src = "../images/photo-red.jpg";
+    ipod1.addEventListener("click", function() {
+        if (isCuijianPlaying) {
+            handleCuijianPlay();
         } else {
+            handleCuijianPlay();
+        }
+    });
+    
+    ipod2.addEventListener("click", function() {
+        if (isBlackkeysPlaying) {
+            handleBlackkeysPlay();
+        } else {
+            handleBlackkeysPlay();
+        }
+    });
+    
+    ipod3.addEventListener("click", function() {
+        if (isBeatlesPlaying) {
+            handleBeatlesPlay();
+        } else {
+            handleBeatlesPlay();
+        }
+    });
+
+    function handleCuijianPlay() {
+        if (isCuijianPlaying) {
             cuijian.pause();
+            isCuijianPlaying = false;
             myImage.src = "../images/photo-bw.jpg";
-        }
-    });
-
-    ipod2.addEventListener("click", function(){
-        if (playingAudio = blackkeys) {
-            blackkeys.play();
-            playingAudio = blackkeys;
-            !cuijian.paused == cuijian.pause();
-            !beatles.paused == beatles.pause();
-            myImage.src = "../images/photo-brown.jpg";
         } else {
+            if (isBlackkeysPlaying || isBeatlesPlaying) {
+                blackkeys.pause();
+                beatles.pause();
+                isBlackkeysPlaying = false;
+                isBeatlesPlaying = false;
+            }
+            cuijian.play();
+            isCuijianPlaying = true;
+            myImage.src = "../images/photo01.jpg";
+        }
+    }
+    
+    function handleBlackkeysPlay() {
+        if (isBlackkeysPlaying) {
             blackkeys.pause();
+            isBlackkeysPlaying = false;
             myImage.src = "../images/photo-bw.jpg";
-        }
-    });
-
-    ipod3.addEventListener("click", function(){
-        if (playingAudio = beatles) {
-            beatles.play();
-            playingAudio = beatles;
-            !cuijian.paused == cuijian.pause();
-            !blackkeys.paused == blackkeys.pause();
-            myImage.src = "../images/photo-yellow.jpg";
         } else {
-            beatles.pause();
-            myImage.src = "../images/photo-bw.jpg";
+            if (isCuijianPlaying || isBeatlesPlaying) {
+                cuijian.pause();
+                beatles.pause();
+                isCuijianPlaying = false;
+                isBeatlesPlaying = false;
+            }
+            blackkeys.play();
+            isBlackkeysPlaying = true;
+            myImage.src = "../images/photo02.jpg";
         }
-    });
+    }
+    
+    function handleBeatlesPlay() {
+        if (isBeatlesPlaying) {
+            beatles.pause();
+            isBeatlesPlaying = false;
+            myImage.src = "../images/photo-bw.jpg";
+        } else {
+            if (isCuijianPlaying || isBlackkeysPlaying) {
+                cuijian.pause();
+                blackkeys.pause();
+                isCuijianPlaying = false;
+                isBlackkeysPlaying = false;
+            }
+            beatles.play();
+            isBeatlesPlaying = true;
+            myImage.src = "../images/photo03.jpg";
+        }
+    }
 });
