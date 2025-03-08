1ef16a135d6aa7c30b7b72aaa5a15dd9bc759785
Meredith Whisman <mwhismain@gmail.com>
2025-03-07 18:51:36 -0800

    updated files

--- game/final/script.js
+++ game/final/script.js
@@ -9,11 +9,32 @@
     const startBtn = document.querySelector(`#lets-see`);
     const attackBtn = document.querySelector(`#attack`);
     const quitBtn = document.querySelector(`#quit`);
+    const volumeBtn = document.querySelector(`#volume`);
 
+    //GAME AUDIO
+    //const cuijian = new Audio("../music/nothingtomyname_cuijian.mp3");
+    const gameAudio = new Audio("music/underclocked-ericskiff.mp3");
+    let isGameAudioPlaying = false;
+    gameAudio.loop = true;
+
+    volume.addEventListener("click", function() {
+        if (!isGameAudioPlaying) {
+            gameAudio.play();
+            isGameAudioPlaying = true;
+            volumeBtn.src = "../images/volume-on.png"
+        } else {
+            gameAudio.pause();
+            isGameAudioPlaying = false;
+            volumeBtn.src = "../images/volume-off.png";
+        }
+    });
+
+    //QUIT BUTTON
     document.querySelector(`#quit`).addEventListener("click", function(){
         window.location.href = "start.html";
     });
 
+    //GAME INFORMATION
     /* These variables are assigned later and used to keep track of the state of the game. Attacker and defender will end up just being the name of the monster for the person who is attacking and the person who is defending. DefenderIndex will be 0 or 1, whichever player is not attacking. */
     let attacker;
     let defender;
@@ -42,7 +63,7 @@
 
     startBtn.addEventListener("click", function(){
         gameData.index = Math.round(Math.random());
-        messages.innerHTML = `<p>Get ready! <strong>${gameData.cats[gameData.index]}</strong> was randomly selected to attack first. Click the attack button to see what happens.</p>`;
+        messages.innerHTML = `<p>Get ready! <strong>${gameData.cats[gameData.index]}</strong> became agitated. Click the attack button to see what happens.</p>`;
         attackBtn.className = "showing";
     });
 
@@ -74,6 +95,7 @@
         //put the correct attack class on the attacking monster
         if(document.querySelector(`#${attacker}`)) {
             document.querySelector(`#${attacker}`).className = `attack${thisAttack}`;
+            document.querySelector(`#${attacker}`).style.color = "green";
         } else {
             console.error(`Element with ID ${attacker} not found`);
         }
@@ -85,10 +107,10 @@
         setTimeout(function(){
             // update the messages div with a defense message
             messages.innerHTML = `<p><strong>${defender}</strong> has ${gameData.defendMessage[thisdDefense]}</p>`;
-            // put the correct defense animation on the defending monster
+            // put the correct defense animation on the defending cat
             document.querySelector(`#${defender}`).className = `defend${thisdDefense}`;
 
-            /* If there is no defense, update the healthbar of the defending monster by subtracting total attack from it's current health. If there was partial defense subtract half the health. If a 2 was rolled, nothing happens to the healthbar, because it was total defense. */
+            /* If there is no defense, update the healthbar of the defending cat by subtracting total attack from it's current health. If there was partial defense subtract half the health. If a 2 was rolled, nothing happens to the healthbar, because it was total defense. */
             if( thisdDefense == 0){
                 gameData.health[defenderIndex] = gameData.health[defenderIndex] - gameData.attack[thisAttack];
             } else if( thisdDefense == 1 ){
@@ -105,9 +127,7 @@
             // set the number next to the health bar to reflect the correct percentage.
             document.querySelector(`#health${defenderIndex}`).innerHTML = `${health}%`;
 
-            /* check to see if the attacking monster has won, passing in the index of the dending
-            monster, so you can check it's health and the name of the attacking monster, so you can
-            use it in the message. */
+            /* check to see if the attacking cat has won, passing in the index of the defending cat, so you can check it's health and the name of the attacking cat, so you can use it in the message. */
             checkWinningCondition(defenderIndex, attacker);
         }, 2500);
     }
@@ -115,7 +135,7 @@
     function checkWinningCondition(enemy, attackingCat){
         //Wait three seconds, so users can read the screen and the animation can finish.
         setTimeout(function(){
-            //Remove the animation classes from the monsters...
+            //Remove the animation classes from the cats...
             // cat1.removeAttribute("class");
             // cat2.removeAttribute("class");
             if (cat1 && cat1.nodeType === Node.ELEMENT_NODE) {
@@ -124,9 +144,9 @@
             if (cat2 && cat2.nodeType === Node.ELEMENT_NODE) {
                 cat2.removeAttribute("class");
             }
-            /* As before, we need to make sure the health of the defending monster is converted into a number and rounded down. */
+            /* As before, we need to make sure the health of the defending cat is converted into a number and rounded down. */
             const health = Math.floor(parseFloat(gameData.health[enemy]));
-            // If the health is less than 1, the winning condition has been met for the attacking monster
+            // If the health is less than 1, the winning condition has been met for the attacking cat
             if( health < 1 ){
                 /* Set the message about winning */
                 messages.innerHTML = `<p><strong>${attackingCat}</strong> has won the battle! Start another battle?</p>`;
